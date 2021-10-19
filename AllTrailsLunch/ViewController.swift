504277acf314799605c48cae67335c3816802931
Alex Carroll <alexmcarroll28@gmail.com>
2021-10-18 20:24:14 -0400

    Initial Commit

---
+++ AllTrailsLunch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AllTrailsLunch
+//
+//  Created by Alex Carroll on 10/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
